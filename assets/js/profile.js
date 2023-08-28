16060706808433594f36e26c8d5b2db54682321a
xunvyre <victoria@thedavids.us>
2023-08-28 17:36:53 -0400

    modal work-- started using AzureDevops so updates may be less frequent here

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,10 +1,83 @@
-//Claim Open Shift Variables
-var openShiftModal = document.getElementById("open-shift-modal");
-var getOpenShiftBtns = document.querySelectorAll(".claim-open-shift");
-var openShiftConfirmBtn = document.getElementById("open-shift-confirm");
-var openShiftCloseBtn = document.getElementById("open-shift-close");
+//request cover variables
+const coverReqModal = document.getElementById("req-cover-modal");
+const getCoverReqBtns = document.querySelectorAll(".req-cover");
+const coverReqConfirmBtn = document.getElementById("req-cover-confirm");
+const coverReqCloseBtn = document.getElementById("req-cover-close");
+const coverReqDisclaimer = document.getElementById("req-cover-msg");
 
-//Claim Open Shift Visual Functions
+//request trade variables
+const tradeReqModal = document.getElementById("req-trade-modal");
+const getTradeReqBtns = document.querySelectorAll(".req-trade");
+const tradeReqConfirmBtn = document.getElementById("req-trade-confirm");
+const tradeReqCloseBtn = document.getElementById("req-trade-close");
+const tradeReqDisclaimer = document.getElementById("req-trade-msg");
+
+//claim open shift variables
+const openShiftModal = document.getElementById("open-shift-modal");
+const getOpenShiftBtns = document.querySelectorAll(".claim-open-shift");
+const openShiftConfirmBtn = document.getElementById("open-shift-confirm");
+const openShiftCloseBtn = document.getElementById("open-shift-close");
+
+//claim cover variables
+const coverShiftModal = document.getElementById("claim-cover-modal");
+const getCoverShiftBtns = document.querySelectorAll(".claim-cover");
+const coverShiftConfirmBtn = document.getElementById("cover-shift-confirm");
+const coverShiftCloseBtn = document.getElementById("cover-shift-close");
+
+//offer trade variables
+const tradeShiftModal = document.getElementById("offer-trade-modal");
+const getTradeShiftBtns = document.querySelectorAll(".offer-trade");
+const tradeShiftConfirmBtn = document.getElementById("trade-shift-confirm");
+const tradeShiftClearBtn = document.getElementById("trade-shift-clear");
+const tradeShiftCloseBtn = document.getElementById("trade-shift-close");
+
+//request cover visual functions
+
+function coverReqActivate()
+{
+    coverReqModal.style.display = "inline-block";
+};
+
+function coverReqConfirm()
+{
+    //if coverReqDisclaimer checked then close, else throw alert
+    console.log("Nothing to see here.")
+};
+
+function coverReqClose()
+{
+    coverReqModal.style.display = "none";
+};
+
+getCoverReqBtns.forEach((btn) =>
+{
+    btn.addEventListener("click", coverReqActivate)
+});
+
+//request trade visual functions
+
+function tradeReqActivate()
+{
+    tradeReqModal.style.display = "inline-block";
+};
+
+function tradeReqConfirm()
+{
+    //if coverReqDisclaimer checked then close, else throw alert
+    console.log("Nothing to see here.")
+};
+
+function tradeReqClose()
+{
+    tradeReqModal.style.display = "none";
+};
+
+getTradeReqBtns.forEach((btn) =>
+{
+    btn.addEventListener("click", tradeReqActivate)
+});
+
+//claim open shift visual functions
 function openShiftActivate()
 {
     openShiftModal.style.display = "inline-block";
@@ -26,5 +99,65 @@ getOpenShiftBtns.forEach((btn) =>
     btn.addEventListener("click", openShiftActivate);
 });
 
+//claim cover visual functions
+function coverShiftActivate()
+{
+    coverShiftModal.style.display = "inline-block";
+};
+
+function coverShiftConfirm()
+{
+    coverShiftModal.style.display = "none";
+    console.log("Sent for approval!");
+};
+
+function coverShiftClose()
+{
+    coverShiftModal.style.display = "none";
+};
+
+getCoverShiftBtns.forEach((btn) =>
+{
+    btn.addEventListener("click", coverShiftActivate)
+});
+
+//offer trade visual functions
+
+function tradeShiftActivate()
+{
+    tradeShiftModal.style.display = "inline-block";
+};
+
+function tradeShiftConfirm()
+{
+    tradeShiftModal.style.display = "none";
+    console.log("Sent for approval!")
+};
+
+function tradeShiftClear()
+{
+    console.log("Reset modal.")
+};
+
+function tradeShiftClose()
+{
+    tradeShiftModal.style.display = "none";
+};
+
+getTradeShiftBtns.forEach((btn) =>
+{
+    btn.addEventListener("click", tradeShiftActivate)
+});
+
+//function calls
+coverReqConfirmBtn.addEventListener("click", coverReqConfirm);
+coverReqCloseBtn.addEventListener("click", coverReqClose);
+tradeReqConfirmBtn.addEventListener("click", tradeReqConfirm);
+tradeReqCloseBtn.addEventListener("click", tradeReqClose);
 openShiftConfirmBtn.addEventListener("click", openShiftConfirm);
 openShiftCloseBtn.addEventListener("click", openShiftClose);
+coverShiftConfirmBtn.addEventListener("click", coverShiftConfirm);
+coverShiftCloseBtn.addEventListener("click", coverShiftClose);
+tradeShiftConfirmBtn.addEventListener("click", tradeShiftConfirm);
+tradeShiftClearBtn.addEventListener("click", tradeShiftClear);
+tradeShiftCloseBtn.addEventListener("click", tradeShiftClose);
